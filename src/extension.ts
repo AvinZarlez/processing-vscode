a4dbd47ef5d2fb5fd727357b4480f79ba2bf954a
Tobiah Zarlez <Tobiah@PlayPerro.com>
2016-01-27 18:44:03 -0800

    Attempting to make a txt file

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,15 @@ export function activate(context: vscode.ExtensionContext) {
 	//Tell the user the extension has been activated.
 	console.log('Processing language extension is now active!'); 
     
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
+var fs = require('fs');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-	
-	context.subscriptions.push(disposable);
+fs.writeFile(context.rootPath+"/test.txt", "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
 	
 }
 

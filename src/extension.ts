10b52672f32c64e39d2300f07904ef76aee6541b
Tobiah Zarlez <Tobiah@PlayPerro.com>
2016-01-28 18:10:31 -0800

    Added "Create Processing Task File"

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,55 @@
 import * as vscode from 'vscode';
 
+var fse = require('fs-extra');
+
+function remindAddToPath() {
+    return vscode.window.showInformationMessage("Remember to add Processing to your path!", "Learn More").then((item) => {
+        if (item === "Learn More") {
+            //Open a URL using the npm module "open"
+            let open = require("open");
+            open("https://github.com/TobiahZ/processing-vscode#add-processing-to-path")
+        }
+    });
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
     console.log('Processing language extension is now active!');
 
     var disposable = vscode.commands.registerCommand('extension.createTaskFile', () => {
-        // The code you place here will be executed every time your command is executed
-        
-        var root = vscode.workspace.rootPath;
 
-        console.log("Root is" + root);
-        if (root == undefined) {
+        var pdeTaskFile = context.extensionPath + "/ProcessingTasks.json";
 
-            console.log('Writing file...');
-            var fs = require('fs');
-
-            fs.writeFile(root + "/test.txt", "Hey there!", function(err) {
-                if (err) {
-                    return console.log(err);
+        if (vscode.workspace.rootPath == undefined) {
+            vscode.window.showErrorMessage("Open project folder first");
+        } else {
+            var taskPath = vscode.workspace.rootPath + "/.vscode/tasks.json";
+            console.log('checking if exists');
+            fse.stat(taskPath, (err, stats) => {
+                if (err && err.code === 'ENOENT') {
+                    // Task file doesn't exist, creating it
+                    fse.copy(pdeTaskFile, taskPath, function(err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        remindAddToPath();
+                    });
+                } else if (err) {
+                    vscode.window.showErrorMessage("When checking if tasks.json exists: " + err);
+                } else if (stats.isFile()) {
+                    return vscode.window.showErrorMessage("tasks.json already exists. Overwrite it?", "Yes").then((item) => {
+                        if (item === "Yes") {
+                            fse.copy(pdeTaskFile, taskPath, function(err) {
+                                if (err) {
+                                    return console.log(err);
+                                }
+                                remindAddToPath();
+                            });
+                        }
+                    });
                 }
-            });
-            console.log("The file was saved!");
+            }
         }
-
-      
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
